e9896c2e1e3ade2d78d96a8973e5048540057e5d
Jana Moudrá <jana.moudra@gmail.com>
2015-10-22 01:13:40 +0200

    Json generator code clean up.

--- tool/json_generator.dart
+++ tool/json_generator.dart
@@ -3,83 +3,103 @@ library json_generator;
 import 'dart:io';
 import 'dart:convert';
 
-const String SOURCE_SESSIONS_FILE_NAME = "sessions-source.json";
-const String SOURCE_SPEAKERS_FILE_NAME = "speakers-source.json";
-const String SESSIONS_FILE_NAME = "sessions.json";
-const String SPEAKERS_FILE_NAME = "speakers.json";
+/// This script takes two source JSON files for sessions and speakers and
+/// generates new JSON files with updated structure which are then used
+/// by server.
 
-Uri getPath(String fileName) =>
-    Platform.script.resolve(fileName);
+const String sourceSessionsFileName = "sessions-source.json";
+const String sourceSpeakersFileName = "speakers-source.json";
+const String sessionsFileName = "sessions.json";
+const String speakersFileName = "speakers.json";
 
-/// Generator generates updated json files which are then loaded by server.
+/// Returns Uri to [fileName] relative to the running script.
+Uri getPath(String fileName) => Platform.script.resolve(fileName);
+
+/// Generator generates updated JSON files which are then loaded by server.
 main() async {
-  File sessionsFile = new File.fromUri(getPath(SOURCE_SESSIONS_FILE_NAME));
-  File speakersFile = new File.fromUri(getPath(SOURCE_SPEAKERS_FILE_NAME));
+  File sessionsFile = new File.fromUri(getPath(sourceSessionsFileName));
+  File speakersFile = new File.fromUri(getPath(sourceSpeakersFileName));
 
   if (sessionsFile.existsSync() && speakersFile.existsSync()) {
     String sessionsContent = await sessionsFile.readAsString();
     String speakersContent = await speakersFile.readAsString();
 
-    //we know that contents are JSON
+    //we know that content of files is a JSON - we decode them
     List sessions = JSON.decode(sessionsContent);
     List speakers = JSON.decode(speakersContent);
 
-    speakers.forEach((Map speaker) {
-      if (speaker.containsKey("featured")) {
-        speaker.remove("featured");
-      }
-
-      if (speaker.containsKey("photoUrl")) {
-        speaker.remove("photoUrl");
-      }
-
-      if (speaker.containsKey("tags")) {
-        speaker.remove("tags");
-      }
-
-      if (speaker.containsKey("socials")) {
-        speaker.remove("socials");
-      }
-
-      if (speaker.containsKey("badges")) {
-        speaker.remove("badges");
-      }
-    });
-
-    sessions.forEach((Map session) {
-      if (session.containsKey("presentation")) {
-        session.remove("presentation");
-      }
-
-      if (session.containsKey("video")) {
-        session.remove("video");
-      }
-
-      if (session.containsKey("track")) {
-        session.remove("track");
-      }
-
-      List sessionSpeakers = [];
-      if (session.containsKey("speakers")) {
-        session["speakers"].forEach((int speakerId) {
-          Map sessionSpeaker = speakers
-              .where((speaker) => speaker["id"] == speakerId)
-              .toList()
-              .first;
-          sessionSpeakers.add(sessionSpeaker); //what if there is an error?
-        });
-        session["speakers"] = sessionSpeakers;
-      }
-    });
-
-    File sessionsOutput =
-        new File.fromUri(getPath("../lib/$SESSIONS_FILE_NAME"));
-    sessionsOutput.writeAsStringSync(JSON.encode(sessions));
-
-    File speakersOutput =
-        new File.fromUri(getPath("../lib/$SPEAKERS_FILE_NAME"));
-    speakersOutput.writeAsStringSync(JSON.encode(speakers));
+    _updateSpeakers(speakers);
+    _updateSessions(sessions, speakers);
   } else {
-    //TODO some error
+    print("ERROR: Source .json files don't exist!");
   }
+}
+
+/// Updates speakers JSON - removes unneeded properties.
+/// 
+/// New file is saved into a lib/ folder.
+void _updateSpeakers(List speakers) {
+  speakers.forEach((Map speaker) {
+    if (speaker.containsKey("featured")) {
+      speaker.remove("featured");
+    }
+
+    if (speaker.containsKey("photoUrl")) {
+      speaker.remove("photoUrl");
+    }
+
+    if (speaker.containsKey("tags")) {
+      speaker.remove("tags");
+    }
+
+    if (speaker.containsKey("socials")) {
+      speaker.remove("socials");
+    }
+
+    if (speaker.containsKey("badges")) {
+      speaker.remove("badges");
+    }
+  });
+
+  File output = new File.fromUri(getPath("../lib/$speakersFileName"));
+  output.writeAsStringSync(JSON.encode(speakers));
+}
+
+/// Updates sessions JSON - removes unneeded properties and updates some.
+/// Sessions JSON now contains also [speakers].
+///
+/// New file is saved into a lib/ folder.
+void _updateSessions(List sessions, List speakers) {
+  sessions.forEach((Map session) {
+    if (session.containsKey("presentation")) {
+      session.remove("presentation");
+    }
+
+    if (session.containsKey("video")) {
+      session.remove("video");
+    }
+
+    if (session.containsKey("track")) {
+      session.remove("track");
+    }
+
+    // Finds speakers in speakers JSON by id and adds them also here - to
+    // the appropriate session.
+    // Source JSON file stores only speaker ids here, we will store full
+    // speaker object here.
+    List sessionSpeakers = [];
+    if (session.containsKey("speakers")) {
+      session["speakers"].forEach((int speakerId) {
+        Map sessionSpeaker = speakers
+            .where((speaker) => speaker["id"] == speakerId)
+            .toList()
+            .first;
+        sessionSpeakers.add(sessionSpeaker);
+      });
+      session["speakers"] = sessionSpeakers;
+    }
+  });
+
+  File output = new File.fromUri(getPath("../lib/$sessionsFileName"));
+  output.writeAsStringSync(JSON.encode(sessions));
 }
